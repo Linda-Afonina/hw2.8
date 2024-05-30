75a3923cb21e8f5203ddcb50066320975e5577e3
linda <charming.linda85@gmail.com>
2024-05-30 17:54:43 +0300

    Сделано ДЗ

---
+++ src/main/java/pro/sky/EmployeeBookStreamAPI/Employee.java
@@ -0,0 +1,70 @@
+package pro.sky.EmployeeBookStreamAPI;
+
+import java.util.Objects;
+
+public class Employee implements Comparable<Employee> {
+
+    private final String firstName;
+    private final String lastName;
+    private int department;
+    private int salary;
+    private static int count;
+    private final int id;
+
+    public Employee(String firstName, String lastName, int department, int salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = department;
+        this.salary = salary;
+        this.id = ++count;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                ", id=" + id +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+        return lastName.compareTo(o.getLastName());
+    }
+}
